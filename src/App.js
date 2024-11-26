c80da0bc532fcdb490e566cb3a7e6ea6e352694b
Project React <baopgse181724@fpt.edu.vn>
2024-11-27 05:29:14 +0700

    Update title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          NO MONEY
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Pham Gia Bao
         </a>
       </header>
     </div>
